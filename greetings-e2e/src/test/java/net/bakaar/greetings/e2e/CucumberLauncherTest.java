3a384c1170260df55bad3e4938c12dfd18b0cd81
Stéphane Krattinger (McKratt) <stef.krattinger@gmail.com>
2021-04-01 17:23:58 +0200

    first working e2e test

--- greetings-e2e/src/test/java/net/bakaar/greetings/e2e/CucumberLauncherTest.java
+++ greetings-e2e/src/test/java/net/bakaar/greetings/e2e/CucumberLauncherTest.java
@@ -2,25 +2,20 @@ package net.bakaar.greetings.e2e;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
+import java.io.File;
+
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.DockerComposeContainer;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {
-        "../bdd/features/GreetingsCreation.feature",
-        "../bdd/features/GreetingsUpdate.feature"
-},
-        glue = "net.bakaar.greetings.e2e.glue",
-        tags = "@e2e"
-)
+@CucumberOptions(features = { "../bdd/features/GreetingsCreation.feature",
+                "../bdd/features/GreetingsUpdate.feature" }, glue = "net.bakaar.greetings.e2e.glue", tags = "@e2e")
 public class CucumberLauncherTest {
 
-    public static final PostgreSQLContainer dbContainer = new PostgreSQLContainer("postgres")
-            .withUsername("foo")
-            .withPassword("secret");
-
-    public static final GenericContainer serviceContainer = new GenericContainer("greeting-service")
-            .withEnv("spring.datasource.username", dbContainer.getUsername())
-            .withEnv("spring.datasource.password", dbContainer.getPassword());
+        @ClassRule
+        public static DockerComposeContainer environment = new DockerComposeContainer(
+                        new File("src/test/resources/compose-test.yaml"))
+                        .withExposedService("greetings_1", 8080);
 }
--- greetings-e2e/src/test/java/net/bakaar/greetings/e2e/glue/GreetingsCreationSteps.java
+++ greetings-e2e/src/test/java/net/bakaar/greetings/e2e/glue/GreetingsCreationSteps.java
@@ -1,49 +1,36 @@
 package net.bakaar.greetings.e2e.glue;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
-import static net.bakaar.greetings.e2e.CucumberLauncherTest.dbContainer;
-import static net.bakaar.greetings.e2e.CucumberLauncherTest.serviceContainer;
+import static net.bakaar.greetings.e2e.CucumberLauncherTest.environment;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-
+import static org.hamcrest.Matchers.equalTo;
 
 public class GreetingsCreationSteps {
-    private final RequestSpecification request = given()
-            .baseUri("http://localhost:9080")
-            .basePath("/rest/api/v1/greetings");
-    private Response response;
 
-    @Before
-    void setUp() {
-        await().until(dbContainer::isHealthy);
-        serviceContainer.addEnv("spring.datasource.url", dbContainer.getJdbcUrl());
-    }
+    private final RequestSpecification request = given().log().all(true).contentType("application/json")
+            .filters(new ResponseLoggingFilter()).accept("application/json");
+
+    private Response response;
 
     @When("I create a(n) {word} greeting for {word}")
     public void iCreateAGreetingForName(String type, String name) {
-        response = request
-                .body("""
-                        {
-                           "type": "%s",
-                           "name": "%s"
-                        }""".formatted(type, name))
-                .post();
+        String url = String.format("http://localhost:%d/rest/api/v1/greetings",
+                environment.getServicePort("greetings", 8080));
+        response = request.body("""
+                {
+                   "type": "%s",
+                   "name": "%s"
+                }""".formatted(type, name)).post(url);
     }
 
     @Then("I get the message {string}")
     public void iGetTheMessage(String message) {
-        String returnedMessage = response.body().jsonPath().getString("$.message");
-        assertThat(returnedMessage).isEqualTo(message);
-    }
-
-    @Then("I get an error")
-    public void iGetAnError() {
-        assertThat(response.statusCode()).isEqualTo(400);
+        response.then().body("message", equalTo(message));
     }
 }
