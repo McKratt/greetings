2ba61cbd6f2f8647e456755f105249978df56380
Stephane Krattinger (U116800) <stephane.krattinger@mobi.ch>
2021-07-13 09:50:46 +0200

    test: clean

--- greetings-service/greetings-application/src/test/java/net/bakaar/greetings/application/CucumberLauncherIT.java
+++ greetings-service/greetings-application/src/test/java/net/bakaar/greetings/application/CucumberLauncherIT.java
@@ -2,12 +2,6 @@ package net.bakaar.greetings.application;
 
 import io.cucumber.junit.platform.engine.Cucumber;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = {
-//        "../../bdd/features"
-//},
-//        glue = "net.bakaar.greetings.application.glue",
-//        tags = "not @stat")
 @Cucumber
 public class CucumberLauncherIT {
 }
