d7139bf6965304c1d1fb5dc65352b36070290387
McKratt <stef.krattinger@gmail.com>
2023-01-04 21:18:23 +0100

    add transaction to stat

--- greetings-stat-service/stat-application/src/main/java/net/bakaar/greetings/stat/application/StatApplicationService.java
+++ greetings-stat-service/stat-application/src/main/java/net/bakaar/greetings/stat/application/StatApplicationService.java
@@ -6,17 +6,19 @@ import net.bakaar.greetings.stat.domain.GreetingCreated;
 import net.bakaar.greetings.stat.domain.GreetingsStats;
 import net.bakaar.greetings.stat.domain.StatRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StatApplicationService {
 
     private final StatRepository statRepository;
     private final GreetingsRepository greetingsRepository;
 
-    // TODO make it transactional in a Reactive way...
+    @Transactional
     public Mono<Void> handle(GreetingCreated event) {
         return Mono.zip(
                 greetingsRepository.getGreetingForIdentifier(event.identifier()),
--- greetings-stat-service/stat-bootstrap/src/main/java/net/bakaar/greetings/stat/StatSpringBootApplication.java
+++ greetings-stat-service/stat-bootstrap/src/main/java/net/bakaar/greetings/stat/StatSpringBootApplication.java
@@ -2,8 +2,10 @@ package net.bakaar.greetings.stat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(proxyBeanMethods = false)
+@EnableTransactionManagement
 public class StatSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatSpringBootApplication.class);
