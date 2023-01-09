e78bc196cac5de62e2f045ebdbb3541774997e0e
McKratt <stef.krattinger@gmail.com>
2023-01-09 21:50:48 +0100

    simplify test

--- greetings-stat-service/stat-client/src/test/java/net/bakaar/greetings/stat/rest/client/GreetingsPactConsumerIT.java
+++ greetings-stat-service/stat-client/src/test/java/net/bakaar/greetings/stat/rest/client/GreetingsPactConsumerIT.java
@@ -10,9 +10,6 @@ import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,17 +19,11 @@ import java.util.UUID;
 
 import static java.util.Collections.singletonMap;
 
-@RestClientTest
 @ExtendWith({PactConsumerTestExt.class})
 @PactTestFor(providerName = "greetings-service", pactVersion = PactSpecVersion.V3)
-@ContextConfiguration(classes = GreetingsStatRestClientConfiguration.class)
 class GreetingsPactConsumerIT {
-
     private final String stringIdentifier = "03e805ff-5860-49a6-88bc-a1dcda0dd4b4";
     private final UUID identifier = UUID.fromString(stringIdentifier);
-    @Autowired
-    private WebClient.Builder builder;
-
 
     @DynamicPropertySource
     static void registerPgProperties(DynamicPropertyRegistry registry) {
