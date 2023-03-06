28a8e05fb6fe34f8b4cef73bf91990b30e713065
McKratt <stef.krattinger@gmail.com>
2023-03-06 11:51:17 +0100

    :fire: nx welcome component

--- greetings-ui/src/app/app.module.ts
+++ greetings-ui/src/app/app.module.ts
@@ -1,13 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {NxWelcomeComponent} from './nx-welcome.component';
-import {RouterModule} from '@angular/router';
-import {appRoutes} from './app.routes';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
