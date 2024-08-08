5d82417231e677f1bb8606fdfa3cf96afb002e78
McKratt <stef.krattinger@gmail.com>
2024-08-08 08:57:15 +0200

    init routing

---
+++ greetings-ui/src/router.ts
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory} from "vue-router";
+import GreetingForm from "./views/GreetingForm.vue";
+
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/form',
+        },
+        {
+            path: '/form',
+            component: GreetingForm
+        },
+        {
+            path: '/stats',
+            component: () => import('./views/Stats.vue'),
+        }
+    ]
+})
