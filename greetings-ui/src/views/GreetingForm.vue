4560f31ce71706b606148dee3d9f1cfbd7452441
McKratt <stef.krattinger@gmail.com>
2025-05-18 09:47:34 +0200

    migrate to tailwind 4 and update test

--- greetings-ui/src/views/GreetingForm.vue
+++ greetings-ui/src/views/GreetingForm.vue
@@ -43,6 +43,6 @@ function submit(): void {
 }
 
 .form-button {
-  @apply align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none
+  @apply align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none
 }
 </style>
