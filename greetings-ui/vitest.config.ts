11ce68546d0e1a99534a5b86884c1eda712494f9
McKratt <stef.krattinger@gmail.com>
2025-05-18 10:28:29 +0200

    CI enable vuejs branch

--- greetings-ui/vitest.config.ts
+++ greetings-ui/vitest.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
-import {fileURLToPath} from 'url';
 
 export default defineConfig({
     plugins: [vue()],
@@ -8,10 +7,5 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         include: ['tests/**/*.spec.ts'],
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
     },
 });
